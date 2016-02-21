4447957d6774d0536ea2e9eb59efe0e161d30214
YangjunLu <youngjunelu@gmail.com>
2016-02-21 17:27:55 -0500

    finish chapter 8

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,7 @@ class Product < ActiveRecord::Base
           %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
-
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
